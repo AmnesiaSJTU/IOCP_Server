7121416459d29a31ad45178e52e0d17a4c168033
qijieyang <solyyang@qq.com>
2020-11-14 22:45:16 +0800

    [Client] add client class functions
    Signed-off-by: qijieyang <solyyang@qq.com>

--- ClientTest/Client.cpp
+++ ClientTest/Client.cpp
@@ -3,3 +3,145 @@
 //
 
 #include "Client.h"
+#include <WinSock2.h>
+
+
+#define RELEASE_POINTER(x)      {if(x!=NULL) {delete(x);x=NULL;}}
+#define RELEASE_HANDLE(x)       {if(x!=nullptr && x!=INVALID_HANDLE_VALUE)\
+    {CloseHandle(x); x = nullptr;}}
+#define RELEASE_ARRAY(x)        {if(x!=nullptr) {delete[] x;x=nullptr;}}
+
+
+
+CClient::CClient(void) :
+    m_strServerIP(DEFAULT_IP),
+    m_strLocalIP(DEFAULT_IP),
+    m_nThreads(DEFAULT_THREADS),
+    m_nPort(DEFAULT_PORT),
+    m_strMessage(DEFAULT_MESSAGE),
+    m_nTimes(DEFAULT_TIMES),
+    m_pMain(NULL),
+
+    m_hShutdownEvent(NULL),
+    m_hConnectionThread(NULL)
+{
+
+}
+
+CClient::~CClient(void)
+{
+    this->stop();
+}
+
+DWORD WINAPI CClient::_ConnectionThread(LPVOID lpParam)
+{
+    ConnectionThreadParam* pParams = (ConnectionThreadParam*)lpParam;
+    CClient* pClient = pParams->pClient;
+    pClient->ShowMessage("Start connection to the server...");
+    pClient->EstablishConnections();
+    pClient->ShowMessage("Connection is over!");
+    RELEASE_POINTER(pParams);
+    return 0;
+}
+
+DWORD WINAPI CClient::_WorkerThread(LPVOID lpParam)
+{
+
+}
+
+void NTAPI CClient::poolThreadWork(
+    _Inout_ PTP_CALLBACK_INSTANCE Instance,
+    _Inout_opt_ PVOID Context,
+    _Inout_ PTP_WORK Work)
+{
+
+}
+
+bool CClient::EstablishConnections()
+{
+    DWORD nThreadId = 0;
+    PCSTR pData = m_strMessage.GetString();
+
+    m_pWorkerParams = new WorkerThreadParam[m_nThreads];
+    ASSERT(m_pWorkerParams != 0);
+    memset(m_pWorkerParams, 0, sizeof(WorkerThreadParam)*m_nThreads);
+
+    // Thread pool operations
+    InitializeThreadpoolEnvironment(&te);
+    threadPool = CreateThreadpool(NULL);
+    BOOL bRet = SetThreadpoolThreadMinimum(threadPool, 2);
+    SetThreadpoolThreadMaximum(threadPool, m_nThreads);
+    SetThreadpoolCallbackPool(&te, threadPool);
+    cleanupGroup = CreateThreadpoolCleanupGroup();
+    SetThreadpoolCallbackCleanupGroup(&te, cleanupGroup, NULL);
+    pWorks = new PTP_WORK[m_nThreads];
+    ASSERT(pWorks != 0);
+
+    for (int i = 0; i < m_nThreads; i++)
+    {
+        if (WAIT_OBJECT_0 == WaitForSingleObject(m_hShutdownEvent, 0))
+        {
+            this->ShowMessage("Receive shutdown command from user!\n");
+            return true;
+        }
+
+        if (!this->ConnectToServer(m_pWorkerParams[i].sock, m_strServerIP, m_nPort))
+        {
+            this->ShowMessage(_T("Connect server failed at %d time", i));
+            continue;
+        }
+
+        m_pWorkerParams[i].nSendTimes = m_nTimes;
+        m_pWorkerParams[i].nThreadNo = i + 1;
+        m_pWorkerParams[i].pClient = this;
+        snprintf(m_pWorkerParams[i].szSendBuffer, MAX_BUFFER_LENGTH,
+            "%s", pData);
+
+        pWorks[i] = CreateThreadpoolWork(poolThreadWork,
+            (PVOID*)(m_pWorkerParams[i]), &te);
+        if (pWorks[i] != NULL)
+        {
+            SubmitThreadpoolWork(pWorks[i]);
+        }
+        Sleep(10);
+    }
+
+    return true;
+}
+
+bool CClient::ConnectToServer(SOCKET* pSocket, CSTRING strServer, int nPort)
+{
+
+}
+
+bool CClient::LoadSocketLib()
+{
+
+}
+
+void CClient::UnloadSocketLib()
+{
+
+}
+
+bool CClient::Start()
+{
+    m_hShutdownEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+    m_nRunningWorkerThreads = 0;
+    DWORD nThreadId = 0;
+    ConnectionThreadParam* pThreadParams = new ConnectionThreadParam;
+    pThreadParams->pClient = this; //
+    m_hConnectionThread = ::CreateThread(0, 0, _ConnectionThread,
+        (void*)pThreadParams, 0, &nThreadId);
+    return true;
+}
+
+void CClient::Stop()
+{
+
+}
+
+void CClient::Cleanup()
+{
+
+}
--- ClientTest/Client.h
+++ ClientTest/Client.h
@@ -3,8 +3,36 @@
 //
 
 #pragma once
+#include <string>
+using std::string
+
+// TODO: need to check if 8*1024 can work or not
+#define MAX_BUFFER_LENGTH (512)
+#define DEFAULT_IP _T("127.0.0.1")
+#define DEFAULT_PORT 10240
+#define DEFAULT_THREADS 10000
+#define DEFAULT_TIMES 10000
+#define DEFAULT_MESSAGE _T("TestMessage!")
+
 
 class CClient;
+
+// re_consider
+struct WorkerThreadParam
+{
+    CClient* pClient;
+    SOCKET   sock;
+    int      nThreadNo;
+    int      nSendTimes;
+    char     szSendBuffer[MAX_BUFFER_LENGTH];
+    char     szRecvBuffer[MAX_BUFFER_LENGTH];
+};
+
+//
+struct ConnectionThreadParam
+{
+    CClient* pClient;
+};
 
 class CClient
 {
@@ -30,11 +58,20 @@ public:
 
     void SetThreads(const int& n) {m_nThreads = n;}
 
+    void SetMessage(const CString& strMessage) {m_strMessage = strMessage;}
 
+    void SetMainDlg(CDialog* p) {m_pMain = p;}
 
 private:
     bool EstablishConnections();
-    
+
+    bool ConnectToServer(SOCKET* pSocket, CString strServer, int nPort);
+
+    static DWORD WINAPI _ConnectionThread(LPVOID lpParam);
+
+    static DWORD WINAPI _WorkerThread(LPVOID lpParam);
+
+    void CleanUp();
 
 private:
     CDialog* m_pMain;
@@ -45,11 +82,22 @@ private:
     int      m_nTimes;
     int      m_nThreads;
 
+    WorkerThreadParam* m_pWorkerParams;
     HANDLE   m_hConnectionThread;
     HANDLE   m_hShutdownEvent;
     LONG     m_nRunningWorkerThreads;
 
     // Thread pool related
+    TP_CALLBACK_ENVIRON te;
+    PTP_POOL threadPool;
+    PTP_CLEANUP_GROUP cleanupGroup;
+    PTP_WORK* pWorks;
 
+    static void NTAPI poolThreadWork(
+        _Inout_ PTP_CALLBACK_INSTANCE Instance,
+        _Inout_opt_ PVOID Context,
+        _Inout_ PTP_WORK Work);
 
+public:
+    void ShowMessage(const char* szFormat, ...);
 };
